7ef9742a8f89126caab72f0c76fe4b1bcb427cef
Carlos Falquez <c.falquez@fz-juelich.de>
2025-01-18 08:57:57 +0100

    g5dbc: Updated statistics parser

--- src/g5dbc/parser/flatjs.py
+++ src/g5dbc/parser/flatjs.py
@@ -1,13 +1,14 @@
 from .parser import StatsParser
 
+
 class FlatJS(StatsParser):
 
-    def update_column(self, r:dict, c: str, key, val) -> dict:
+    def update_column(self, r: dict, c: str, key, val) -> dict:
         if key is None:
             try:
                 r[c] = val + r.get(c, 0)
             except Exception as e:
-                print(f"parser warning: col={c} err={e}")
+                print(f"Warning: col={c} err={e}")
         elif key == "total" or key == "samples":
             c = f"{c}_total"
             r[c] = val + r.get(c, 0)
@@ -15,11 +16,14 @@ class FlatJS(StatsParser):
             c = f"{c}_sum"
             r[c] = val + r.get(c, 0)
         elif key == "values" or key == "list":
-            for k,v in val:
+            for k, v in val:
                 d = r.setdefault(c, dict())
-                d[k] = v + d.get(k,0)
+                d[k] = v + d.get(k, 0)
         else:
             d = r.setdefault(c, dict())
-            d[key] = val + d.get(key,0)
-        
+            d[key] = val + d.get(key, 0)
+
+        return r
+
+    def update_terminal_output(self, r: dict, parsed_output: dict) -> dict:
         return r
--- src/g5dbc/parser/parser.py
+++ src/g5dbc/parser/parser.py
@@ -1,33 +1,35 @@
+import re
 from abc import ABC, abstractmethod
 from pathlib import Path
-import re
+
 import yaml
 
-from ..util.parser import stats_line_generator, parse_number_text
 from ..config import Config
+from ..util.parser import parse_number_text, stats_line_generator
+
 
 class StatsParser(ABC):
     """
     Statistics Parser
     """
+
     def __init__(self, parser_re_dir: Path):
-
         parser_conf = [f for f in parser_re_dir.iterdir() if f.suffix == ".yaml"]
-
         self.regexes = []
-
         for file in parser_conf:
-            with file.open('r') as stream:
+            with file.open("r") as stream:
                 self.regexes.extend(yaml.safe_load(stream))
 
-
-    def parse_stats(self, stats_params: dict, parsed_output: dict, stats_file: Path) -> list[dict[str,int|float|list]]:
-        max_roi_id=5
-
-        parsed_rois = [dict(**stats_params, roi_id=roi_id) for roi_id in range(max_roi_id)]
+    def parse_stats(
+        self, stats_params: dict, parsed_output: dict, stats_file: Path
+    ) -> list[dict[str, int | float | list]]:
+        max_roi_id = 5
+        parsed_rois = [
+            dict(**stats_params, roi_id=roi_id) for roi_id in range(max_roi_id)
+        ]
         for roi_id, path, key, val in stats_line_generator(stats_file):
-            s = '.'.join(path)
-            for r,labels in self.regexes:
+            s = ".".join(path)
+            for r, labels in self.regexes:
                 m = re.fullmatch(r, s)
                 if m:
                     for l in labels:
@@ -36,54 +38,18 @@ class StatsParser(ABC):
                         self.update_column(parsed_rois[roi_id], c, key, val)
 
         for roi_id, parsed_roi in enumerate(parsed_rois):
-            self.normalize_network(parsed_roi, parsed_output)
+            self.update_terminal_output(parsed_roi, parsed_output)
 
         return parsed_rois
 
-
-    def normalize_network(self, r: dict, parsed_output:dict) -> dict:
-        if 'simTicks' in r:
-        
-            ruby_cycles   = float(r['simTicks'])/r['interconnect_clock']
-
-            links = parsed_output["output_log"]["network"]["links"]
-            rtp_node = r.setdefault("router_topology", dict())
-
-            int_node = rtp_node.setdefault("internal", dict())
-            exo_node = rtp_node.setdefault("ext_out", dict())
-            exi_node = rtp_node.setdefault("ext_in", dict())
-
-            for link in links:
-                nxt_node = None
-                if link['linkType'] == 'Internal':
-                    nxt_node = int_node
-                elif link['linkType'] == 'ExtIn':
-                    nxt_node = exi_node
-                elif link['linkType'] == 'ExtOut':
-                    nxt_node = exo_node
-                else:
-                    continue
-                linkId = link['linkId']
-                src_node = nxt_node.setdefault(link['srcId'], dict())
-                dst_node = src_node.setdefault(link['dstId'], dict())
-
-                # Append to Link List
-                rt_links = dst_node.setdefault("link", list())
-                rt_links.append(linkId)
-                # Find Link Traffic statistics
-                dst_node.setdefault("total_packets", 0)
-                stats = sorted([c for c in r if re.search(fr"network_network_latencies_vnet(\d+)_link{linkId}_total", c)])
-                for s in stats:
-                    m = re.match(fr"network_network_latencies_vnet(\d+)_link{linkId}_total", s)
-                    vnetId = int(m.group(1))
-                    vnet_t = dst_node.setdefault("vnet", dict())
-                    link_t = vnet_t.setdefault(vnetId, list())
-                    link_t.append(dict(linkId=linkId, samples=r[s], traffic=float(r[s])/ruby_cycles ))
-                    dst_node["total_packets"] += r[s]
-                    dst_node["total_traffic"] = float(dst_node["total_packets"]) / ruby_cycles
+    @abstractmethod
+    def update_column(self, r: dict, c: str, key, val) -> dict:
+        """
+        Update statistics column
+        """
 
     @abstractmethod
-    def update_column(self, r:dict, c: str, key, val) -> dict:
+    def update_terminal_output(self, r: dict, parsed_output: dict) -> dict:
         """
-        Update column
+        Update statistics with information from terminal output
         """
