6e8af742126b373e1283aea40ca44c96f0085199
Carlos Falquez <c.falquez@fz-juelich.de>
2025-01-30 05:06:30 +0100

    g5dbc: Improved find_file

--- src/g5dbc/util/filesystem.py
+++ src/g5dbc/util/filesystem.py
@@ -5,6 +5,8 @@ def find_file(name: str, suffix: str, *paths) -> Path | None:
     fname = Path(name)
     if not fname.suffix:
         fname = Path(f"{name}{suffix}")
+    if fname.exists():
+        return fname.resolve()
     for p in paths:
         if Path(p).joinpath(fname).exists():
             return Path(p).joinpath(fname).resolve()
